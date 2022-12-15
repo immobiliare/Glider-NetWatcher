9241fdefe91488ea9d73987a1751d0cdf70a0abc
Daniele Margutti <hello@danielemargutti.com>
2022-12-15 16:48:08 +0100

    Moved from main repository

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GliderNetWatcher",
+    platforms: [
+        .iOS(.v14), .macOS(.v11), .tvOS(.v14)
+    ],
+    products: [
+        .library(name: "GliderNetWatcher", targets: ["GliderNetWatcher"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/immobiliare/Glider.git", .upToNextMajor(from: "2.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "GliderNetWatcher",
+            dependencies: [
+                "Glider"
+            ],
+            path:"GliderNetWatcher/Sources"
+        ),
+        .testTarget(
+            name: "GliderNetWatcherTests",
+            dependencies: [
+                "Glider",
+                "GliderNetWatcher"
+            ],
+            path: "Tests/GliderNetWatcherTests"
+        )
+    ]
+)
